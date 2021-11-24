b458c0d9ecb5156924d18f1dc03b849a7fb706b5
Benny <benny.bellier@gmail.com>
2021-11-24 10:59:22 +0100

    Correction coquilles dans représentation UML et update README.md

--- data/v0_createDB.sql
+++ data/v0_createDB.sql
@@ -1,27 +1,62 @@
-create table V0_LesRepresentations (
-    noSpec integer not null,
-    nomSpec varchar(50) not null,
-    dateRep date not null,
-    promoRep decimal (4,2) not null,
-    prixBaseSpec decimal (6,2) not null,
-    prixRep decimal (6,2) not null,
-    constraint pk_rep_noSpec_dateRep primary key (noSpec, dateRep),
-    constraint ck_rep_noSpec check (noSpec > 0),
-    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0),
-    constraint ck_spec_prixBaseSpec check (prixRep >= 0),
-    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1)
+CREATE TABLE LESSPECTACLES(
+  nospec INT PRIMARY KEY,
+  nomspec VARCHAR (50) NOT NULL,
+  prixbasespec DECIMAL (6, 2) NOT NULL,
+  CONSTRAINT ck_spec_nospec CHECK (nospec > 0),
+  CONSTRAINT ck_spec_prixbasespec CHECK (prixbasespec >= 0)
 );
-
-create table V0_LesPlaces (
-    noPlace integer,
-    noRang integer,
-    noZone integer not null,
-    catZone varchar (50) not null,
-    tauxZone decimal (4,2) not null,
-    constraint pk_pl_noP_noR primary key (noZone, noPlace, noRang),
-    constraint ck_pl_noP check (noPlace > 0),
-    constraint ck_pl_noR check (noRang > 0),
-    constraint ck_pl_noZone check (noZone > 0),
-    constraint ck_pl_tauxZone check (tauxZone >= 0),
-    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+CREATE TABLE LESREPRESEANTATION(
+  daterep DATE PRIMARY KEY,
+  promorep DECIMAL (4, 2) NOT NULL,
+  CONSTRAINT ck_rep_promorep CHECK (
+    promorep >= 0
+    AND promorep <= 1
+  )
+);
+create table LESVENTES (
+  notrans int primary key,
+  datetrans date NOT NULL,
+  prixticket float NOT NULL,
+  constraint ck_vente_prixticket check (prixticket >= 0)
+)
+CREATE table LESPLACES(
+  noplace INT,
+  norang int,
+  CONSTRAINT pk_places_noplace_norang PRIMARY KEY (noplace, norang)
+)
+create table LESREDUCTIONS(
+  typepers varchar(20) PRIMARY KEY,
+  prix
+)
+/* -------------------------------------------------------------------------------------- */
+CREATE TABLE V0_LESREPRESENTATIONS(
+  nospec integer NOT NULL,
+  nomspec VARCHAR (50) NOT NULL,
+  daterep DATE NOT NULL,
+  promorep DECIMAL (4, 2) NOT NULL,
+  prixbasespec DECIMAL (6, 2) NOT NULL,
+  prixrep DECIMAL (6, 2) NOT NULL,
+  CONSTRAINT pk_rep_nospec_daterep PRIMARY KEY (nospec, daterep),
+  CONSTRAINT ck_rep_nospec CHECK (nospec > 0),
+  CONSTRAINT ck_spec_prixbasespec CHECK (prixbasespec >= 0),
+  CONSTRAINT ck_spec_prixbasespec CHECK (prixrep >= 0),
+  CONSTRAINT ck_rep_promorep CHECK (
+    promorep >= 0
+    AND promorep <= 1
+  )
+);
+CREATE TABLE V0_LESPLACES(
+  noplace integer,
+  norang integer,
+  nozone integer NOT NULL,
+  catzone VARCHAR (50) NOT NULL,
+  tauxzone DECIMAL (4, 2) NOT NULL,
+  CONSTRAINT pk_pl_nop_nor PRIMARY KEY (nozone, noplace, norang),
+  CONSTRAINT ck_pl_nop CHECK (noplace > 0),
+  CONSTRAINT ck_pl_nor CHECK (norang > 0),
+  CONSTRAINT ck_pl_nozone CHECK (nozone > 0),
+  CONSTRAINT ck_pl_tauxzone CHECK (tauxzone >= 0),
+  CONSTRAINT ck_pl_cat CHECK (
+    catzone IN ('orchestre', 'balcon', 'poulailler')
+  )
 );
