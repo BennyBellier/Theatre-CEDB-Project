fc925a7faff996da5c6b33178b5c971b660d5e84
Benfa7 <benjaminfa7@gmail.com>
2021-11-26 14:00:38 +0100

    Ajout de toutes les tables dans v1_createDB.sql

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,30 +1,98 @@
 -- TODO 1.3 : Créer les tables manquantes et modifier celles ci-dessous
-create table LesRepresentations (
+create table LesSpectacles (
     noSpec integer not null,
     nomSpec varchar(50) not null,
+    prixBaseSpec decimal (6,2) not null,
+    constraint pk_rep_noSpec primary key (noSpec),
+    constraint ck_rep_noSpec check (noSpec > 0),
+    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0)
+
+);
+
+create table LesRepresentations (
     dateRep date not null,
     promoRep decimal (4,2) not null,
-    prixBaseSpec decimal (6,2) not null,
-    prixRep decimal (6,2) not null,
-    constraint pk_rep_noSpec_dateRep primary key (noSpec, dateRep),
-    constraint ck_rep_noSpec check (noSpec > 0),
-    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0),
-    constraint ck_spec_prixBaseSpec check (prixRep >= 0),
-    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1)
+    noSpec integer not null,
+    constraint pk_rep_dateRep primary key ( dateRep),
+    constraint ck_rep_noSpec check (noSpec > 0), --pas forcement nécessaire puisqu'il doit verifier qu'il est dans LesSpectacles mais au moins erreur plus précise
+    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1),
+    constraint fk_noSpec foreign key (noSpec)
+    references LesSpectacles(noSpec)
+);
+
+create table LesZones (
+    noZone integer not null,
+    catZone varchar (50) not null,
+    constraint pk_pl_noZ primary key (noZone),,
+    constraint ck_pl_noZone check (noZone > 0),
+    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+);
+
+create table TypeZones (
+    catZone varchar (50) not null,
+    tauxZone decimal (4,2) not null,
+    constraint pk_pl_catZ primary key (catZone),,
+    constraint ck_pl_tauxZone check (tauxZone >= 0),
+    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler')),
+    constraint fk_typZ foreign key (catZone)
+    references LesZones(noZone)
 );
 
 create table LesPlaces (
     noPlace integer,
     noRang integer,
     noZone integer not null,
-    catZone varchar (50) not null,
-    tauxZone decimal (4,2) not null,
-    constraint pk_pl_noP_noR primary key (noZone, noPlace, noRang),
+    constraint pk_pl_noP_noR primary key (noPlace, noRang),
     constraint ck_pl_noP check (noPlace > 0),
     constraint ck_pl_noR check (noRang > 0),
-    constraint ck_pl_noZone check (noZone > 0),
-    constraint ck_pl_tauxZone check (tauxZone >= 0),
-    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+    constraint ck_pl_noZone check (noZone > 0), --PAS FORCEMENT NECESSAIRE ?
+    constraint fk_LesPlaces foreign key (noZone)
+    references LesZones(noZone)
+
+);
+
+create table LesDosssiers (
+    noDossier integer,
+    prixDossier integer,
+    constraint pk_pl_noP_noR primary key (noDossier),
+    constraint ck_pl_PrixD check (prixDossier > 0)
+);
+
+create table LesRéductions (
+    typePers varchar (50) not null,
+    tarifReduit decimal (4,2) not null,
+    constraint pk_pl_LesReducs primary key (typePers),
+    constraint ck_rep_tarifReduit check (promoRep >= 0 and promoRep <=1),
+    constraint ck_pl_typeP check (typePers in ('ordinaire', 'adhérent', 'étudiant','scolaire', 'militaire', 'sénior')),
+    constraint fk_LesPlaces foreign key (noZone)
+    references LesZones(noZone)
+
+);
+
+
+create table LesVentes (
+    noTrans integer,
+    dateTrans date not null,
+    prixTotal integer,
+    noPlace integer,
+    noRang integer,
+    typePers varchar (50) not null,
+    noDossier integer,
+    dateRep date not null,
+    constraint pk_pl_LesV primary key (noTrans),
+    constraint ck_pl_PrixTot check (prixTotal > 0),
+    constraint ck_pl_noP check (noPlace > 0),
+    constraint ck_pl_noR check (noRang > 0),
+    constraint ck_pl_typeP check (typePers in ('ordinaire', 'adhérent', 'étudiant','scolaire', 'militaire', 'sénior')), --PAS FORCEMENT
+    constraint ck_pl_noD check (noDossier > 0),
+    constraint fk_dateRep foreign key (dateRep)
+    references LesRepresentations(dateRep),
+    constraint fk_noP_noR foreign key (noPlace,noRang)
+    references LesPlaces(noPlace,noRang),
+    constraint fk_typeP foreign key (typePers)
+    references LesRéductions(typePers),
+    constraint fk_noD foreign key (noDossier)
+    references LesDosssiers(noD)
 );
 -- TODO 1.4 : Créer une vue LesRepresentations ajoutant le nombre de places disponible et d'autres possibles attributs calculés.
 -- TODO 1.5 : Créer une vue  avec le noDos et le montant total correspondant.
